b95a3ddd54e0d60db3fe2490ca335ef2bd844edb
yaser <yaser.demet@elegantyazilim.com>
2023-02-22 09:29:27 +0300

    featured: ui badge

---
+++ src/components/Badge.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+interface Iprops {
+  text:  number;
+  extraClasses?: string;
+}
+const Badge: React.FC<Iprops> = ({ text, extraClasses }) => {
+  return (
+    <>
+      {text > 0 && (
+        <span
+          className={`${extraClasses && extraClasses} ${
+            Number(text) > 99 ? "w-content px-1" : "w-5"
+          } text-white h-5   flex  justify-center items-center rounded-full  bg-primaryRed  text-xs font-normal`}
+        >
+          {Number(text) > 99 ? String(text) + "+" : text}
+        </span>
+      )}
+    </>
+  );
+};
+
+export default Badge;
